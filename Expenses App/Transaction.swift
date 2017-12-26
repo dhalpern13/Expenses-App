b762f52330f8a045a39f86e94a530f7b0ad1fd47
Daniel Halpern <dhalpern13@gmail.com>
2017-12-26 15:42:35 -0500

    Add User and Transaction Files
    Added files and added outline for Transaction

---
+++ Expenses App/Transaction.swift
@@ -0,0 +1,20 @@
+//
+//  Transaction.swift
+//  Expenses App
+//
+//  Created by Daniel Halpern on 12/26/17.
+//  Copyright © 2017 Christopher Bantle and Daniel Halpern. All rights reserved.
+//
+
+import Foundation
+
+class Transaction {
+    
+    date:Date
+    description:String
+    Amount:double
+    Payer:User
+    
+}
+
+
---
+++ Expenses App/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  Expenses App
+//
+//  Created by Daniel Halpern on 12/26/17.
+//  Copyright © 2017 Christopher Bantle and Daniel Halpern. All rights reserved.
+//
+
+import Foundation
