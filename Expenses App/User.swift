65e501c16d9c7a14269272b5d6d91d7f111e354f
Daniel Halpern <dhalpern13@gmail.com>
2018-01-06 15:37:28 -0500

    Added methods to User

--- Expenses App/User.swift
+++ Expenses App/User.swift
@@ -11,8 +11,8 @@ import Foundation
 class User: Hashable {
     var name: String
     var userName: String
-    var individualTransactions = [String: [Transaction]]()
-    var sharedTransactions = [User: [ObservableTransaction]]()
+    var individualTransactions = [String: [(Transaction, Bool)]]()
+    var sharedTransactions = [User: [(ObservableTransaction, Bool)]]()
     var hashValue: Int {
         return self.userName.hashValue
     }
@@ -23,15 +23,96 @@ class User: Hashable {
     }
     
     func addCategory(_ category: String) {
-        if individualTransactions[category] == nil {
-            individualTransactions[category] = []
+        if self.individualTransactions[category] == nil {
+            self.individualTransactions[category] = []
         }
     }
     
     func addFriend(_ friend: User) {
-        if sharedTransactions[friend] == nil  {
-            sharedTransactions[friend] = []
+        if self.sharedTransactions[friend] == nil  {
+            self.sharedTransactions[friend] = []
         }
+    }
+    
+    func addIndividualTransaction(date: Date, description: String, amount: Decimal, paidFor: Bool, category: String) {
+        let toAdd = (TransactionFactory.getIndvidualTransaction(date: date, description: description, amount: amount), paidFor)
+        if var categoryTransactions = self.individualTransactions[category] {
+            categoryTransactions.append(toAdd)
+        } else {
+            self.individualTransactions[category] = [toAdd]
+        }
+    }
+    
+    func addDirectTransaction(date: Date, description: String, amount: Decimal, paidFor: Bool, to: User) {
+        let transaction = TransactionFactory.getDirectPayment(date: date, description: description, amount: amount)
+        addToSharedTransactions((transaction, paidFor), user: to)
+        to.addToSharedTransactions((transaction, !paidFor), user: self)
+    }
+    
+    func addSplitTransaction(date: Date, description: String, amount: Decimal, paidFor: User, forUsers: [User]) {
+        let transaction = TransactionFactory.getDirectPayment(date: date, description: description, amount: amount)
+        for user in forUsers {
+            paidFor.addToSharedTransactions((transaction, true), user: user)
+            user.addToSharedTransactions((transaction, false), user: paidFor)
+        }
+    }
+    
+    private func addToSharedTransactions(_ transaction: (ObservableTransaction, Bool), user: User) {
+        if var userTransactions = self.sharedTransactions[user] {
+            userTransactions.append(transaction)
+        } else {
+            self.sharedTransactions[user] = [transaction]
+        }
+    }
+    
+    func getTotalExpensesInCategory(_ category: String) -> Decimal {
+        if let categoryTransactions = individualTransactions[category] {
+            var total = Decimal(0)
+            for (transaction, paidFor) in categoryTransactions{
+                if paidFor {
+                    total += transaction.amount
+                } else {
+                    total -= transaction.amount
+                }
+            }
+            return total
+        } else {
+            return Decimal(0)
+        }
+    }
+    
+    func getTotalExpensesOfUser(_ user: User) -> Decimal {
+        if let userTransactions = sharedTransactions[user] {
+            var total = Decimal(0)
+            for (transaction, paidFor) in userTransactions{
+                if paidFor {
+                    total += transaction.amount
+                } else {
+                    total -= transaction.amount
+                }
+            }
+            return total
+        } else {
+            return Decimal(0)
+        }
+    }
+
+
+    
+    func getIndividualBalance() -> Decimal {
+        var total = Decimal(0)
+        for (category, _) in individualTransactions {
+            total += getTotalExpensesInCategory(category)
+        }
+        return total
+    }
+    
+    func getUserBalance() -> Decimal {
+        var total = Decimal(0)
+        for (user, _) in sharedTransactions {
+            total += getTotalExpensesOfUser(user)
+        }
+        return total
     }
     
     
